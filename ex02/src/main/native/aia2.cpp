48953c7b2c12d72dcd82cbb40b1bfb9e304e7c43
Nicolas Brieger <github@xtmp.de>
2012-11-03 12:51:39 +0100

    normFD Initial comments.

--- ex02/src/main/native/aia2.cpp
+++ ex02/src/main/native/aia2.cpp
@@ -39,143 +39,169 @@ void plotFD(Mat& fd, string win, double dur=-1);
  */
 int main(int argc, char** argv) {
 
-	// check if image paths were defined
-	if (argc != 4) {
-		cerr << "Usage: aia2 <input image>  <class 1 example>  <class 2 example>" << endl;
-		return -1;
-	}
+    // check if image paths were defined
+    if (argc != 4) {
+        cerr << "Usage: aia2 <input image>  <class 1 example>  <class 2 example>" << endl;
+        return -1;
+    }
 
-	// process image data base
-	// load image as gray-scale, paths in argv[2] and argv[3]
-	Mat exC1 = imread(argv[2], 0);
-	Mat exC2  = imread(argv[3], 0);
-	if ((!exC1.data) || (!exC2.data)) {
-		cerr << "ERROR: Cannot load class examples in\n" << argv[2] << "\n" << argv[3] << endl;
-		return -1;
-	}
+    // process image data base
+    // load image as gray-scale, paths in argv[2] and argv[3]
+    Mat exC1 = imread(argv[2], 0);
+    Mat exC2  = imread(argv[3], 0);
+    if ((!exC1.data) || (!exC2.data)) {
+        cerr << "ERROR: Cannot load class examples in\n" << argv[2] << "\n" << argv[3] << endl;
+        return -1;
+    }
 
-	// get contour line from images
-	vector<Mat> contourLines1;
-	vector<Mat> contourLines2;
-	// TODO
-	//binThreshold = ???;
-	//numOfErosions = ???;
-	getContourLine(exC1, contourLines1, binThreshold, numOfErosions);
-	getContourLine(exC2, contourLines2, binThreshold, numOfErosions);
+    // get contour line from images
+    vector<Mat> contourLines1;
+    vector<Mat> contourLines2;
 
-	// calculate fourier descriptor
-	Mat fd1 = makeFD(contourLines1.front());
-	Mat fd2 = makeFD(contourLines2.front());
+    getContourLine(exC1, contourLines1, binThreshold, numOfErosions);
+    getContourLine(exC2, contourLines2, binThreshold, numOfErosions);
 
-	// normalize  fourier descriptor
-	// TODO
-	//steps = ???;
-	Mat fd1_norm = normFD(fd1, steps);
-	Mat fd2_norm = normFD(fd2, steps);
+    // calculate fourier descriptor
+    Mat fd1 = makeFD(contourLines1.front());
+    Mat fd2 = makeFD(contourLines2.front());
 
-	// process query image
-	// load image as gray-scale, path in argv[1]
-	Mat query = imread( argv[1], 0);
-	if (!query.data){
-		cerr << "ERROR: Cannot load query image in\n" << argv[1] << endl;
-		return -1;
-	}
+    // normalize  fourier descriptor
+    // TODO
+    //steps = ???;
+    Mat fd1_norm = normFD(fd1, steps);
+    Mat fd2_norm = normFD(fd2, steps);
 
-	// get contour lines from image
-	vector<Mat> contourLines;
-	// TODO
-	//binThreshold = ???;
-	//numOfErosions = ???;
-	getContourLine(query, contourLines, binThreshold, numOfErosions);
+    // process query image
+    // load image as gray-scale, path in argv[1]
+    Mat query = imread( argv[1], 0);
+    if (!query.data){
+        cerr << "ERROR: Cannot load query image in\n" << argv[1] << endl;
+        return -1;
+    }
 
-	cout << "Found " << contourLines.size() << " object candidates" << endl;
+    // get contour lines from image
+    vector<Mat> contourLines;
+    // TODO
+    //binThreshold = ???;
+    //numOfErosions = ???;
+    getContourLine(query, contourLines, binThreshold, numOfErosions);
 
-	// just to visualize classification result
-	Mat result(query.rows, query.cols, CV_8UC3);
-	vector<Mat> tmp;
-	tmp.push_back(query);
-	tmp.push_back(query);
-	tmp.push_back(query);
-	merge(tmp, result);
+    cout << "Found " << contourLines.size() << " object candidates" << endl;
 
-	// loop through all contours found
-	int i=1;
-	// TODO
-	//detThreshold = ???;
-	for (vector<Mat>::iterator c = contourLines.begin(); c != contourLines.end(); c++, i++)	{
+    // just to visualize classification result
+    Mat result(query.rows, query.cols, CV_8UC3);
+    vector<Mat> tmp;
+    tmp.push_back(query);
+    tmp.push_back(query);
+    tmp.push_back(query);
+    merge(tmp, result);
 
-		cout << "Checking object candidate no " << i << " :\t";
+    // loop through all contours found
+    int i=1;
+    // TODO
+    //detThreshold = ???;
+    for (vector<Mat>::iterator c = contourLines.begin(); c != contourLines.end(); c++, i++)	{
 
-		for (int i = 0; i < c->cols; i++) {
-			result.at<Vec3b>(c->at<Vec2i>(0,i)[1], c->at<Vec2i>(0,i)[0]) = Vec3b(255,0,0);
-		}
-		showImage(result, "Current Object", 0);
+        cout << "Checking object candidate no " << i << " :\t";
 
-		// if fourier descriptor has too few components (too small contour), then skip it
-		if (c->cols < steps*2) {
-			cout << "Too less boundary points" << endl;
-			continue;
-		}
+        for (int i = 0; i < c->cols; i++) {
+            result.at<Vec3b>(c->at<Vec2i>(0,i)[1], c->at<Vec2i>(0,i)[0]) = Vec3b(255,0,0);
+        }
+        showImage(result, "Current Object", 0);
 
-		// calculate fourier descriptor
-		Mat fd = makeFD(*c);
+        // if fourier descriptor has too few components (too small contour), then skip it
+        if (c->cols < steps*2) {
+            cout << "Too less boundary points" << endl;
+            continue;
+        }
 
-		// normalize fourier descriptor
-		Mat fd_norm = normFD(fd, steps);
+        // calculate fourier descriptor
+        Mat fd = makeFD(*c);
 
-		// compare fourier descriptors
-		double err1 = norm(fd_norm, fd1_norm) / steps;
-		double err2 = norm(fd_norm, fd2_norm) / steps;
+        // normalize fourier descriptor
+        Mat fd_norm = normFD(fd, steps);
 
-		// if similarity is too small, then reject
-		if (min(err1, err2) > detThreshold){
-			cout << "No class instance ( " << min(err1, err2) << " )" << endl;
-			continue;
-		}
+        // compare fourier descriptors
+        double err1 = norm(fd_norm, fd1_norm) / steps;
+        double err2 = norm(fd_norm, fd2_norm) / steps;
 
-		// otherwise: assign color according to class
-		Vec3b col;
-		if (err1 > err2) {
-			col = Vec3b(0,0,255);
-			cout << "Class 2 ( " << err2 << " )" << endl;
-		} else {
-			col = Vec3b(0,255,0);
-			cout << "Class 1 ( " << err1 << " )" << endl;
-		}
-		for (int i = 0; i < c->cols; i++) {
-			result.at<Vec3b>(c->at<Vec2i>(0,i)[1], c->at<Vec2i>(0,i)[0]) = col;
-		}
+        // if similarity is too small, then reject
+        if (min(err1, err2) > detThreshold){
+            cout << "No class instance ( " << min(err1, err2) << " )" << endl;
+            continue;
+        }
 
-		// for intermediate results, use the following line
-		showImage(result, "Current Object", 0);
-	}
-	// save result
-	imwrite("result.png", result);
-	// show final result
-	showImage(result, "Result", 0);
+        // otherwise: assign color according to class
+        Vec3b col;
+        if (err1 > err2) {
+            col = Vec3b(0,0,255);
+            cout << "Class 2 ( " << err2 << " )" << endl;
+        } else {
+            col = Vec3b(0,255,0);
+            cout << "Class 1 ( " << err1 << " )" << endl;
+        }
+        for (int i = 0; i < c->cols; i++) {
+            result.at<Vec3b>(c->at<Vec2i>(0,i)[1], c->at<Vec2i>(0,i)[0]) = col;
+        }
 
-	return 0;
+        // for intermediate results, use the following line
+        showImage(result, "Current Object", 0);
+    }
+    // save result
+    imwrite("result.png", result);
+    // show final result
+    showImage(result, "Result", 0);
+
+    return 0;
 }
 
 /**
  * normalize a given fourier descriptor
  * @param fd  the given fourier descriptor
  * @param n  number of used frequencies (should be even)
- * @param out  the normalized fourier descriptor
+ * @return the normalized fourier descriptor
  */
-Mat normFD(Mat& fd, int n) {
-
-	// TODO
+Mat normFD(Mat& fd, int n)
+{
+    if(n % 2 != 0)
+    {
+        cerr << "normFD: Used frequencies must be even, is " << n << "." << endl;
+        return fd;
+    }
+    else
+    {
+        Mat out(fd);
+        //translation invariance -> first element to 0
+        //scale invariance -> divide all elements by 2nd element
+        //rotation invariance -> delete phase information using cartToPolar(..)
+        //ignore high frequencies -> delete middle elemenents of vector so that only first and last n/2 elements remain
+    }
 }
 
 /**
  * calculates the (unnormalized) fourier descriptor from a list of points
  * @param contour  1xN 2-channel matrix, containing N points (x in first, y in second channel)
- * @param out  fourier descriptor (not normalized)
+ * @return fourier descriptor (not normalized)
  */
-Mat makeFD(Mat& contour) {
+Mat makeFD(Mat& contour)
+{
+    if(contour.type() == CV_32SC2) //if the input is only integer precision, convert
+        contour.convertTo(contour,CV_32FC2);
 
-	// TODO
+    Size outSize;
+    outSize.width = 1;
+    //outSize.height = getOptimalDFTSize(contour.rows - 1); //could be activated for faster processing
+    outSize.height = contour.rows;
+    Mat tempContour(outSize,
+                    contour.type(), //when used with CV_32FC2, some assertion fails
+                    Scalar::all(0) //initialize with zeros
+                    );
+    //copy contour to tempcontour
+    contour.copyTo(tempContour);
+
+    dft(tempContour,tempContour);
+
+    return tempContour;
 }
 
 /**
@@ -185,9 +211,9 @@ Mat makeFD(Mat& contour) {
  * @param thresh  threshold used to binarize the image
  * @param k  number of applications of the erosion operator
  */
-void getContourLine(Mat& img, vector<Mat>& objList, int thresh, int k){
-
-	//image preparation
+void getContourLine(Mat& img, vector<Mat>& objList, int thresh, int k)
+{
+    //image preparation
     threshold(img,img,thresh,
               255, //set all points meeting the threshold to 255
               THRESH_BINARY //output is a binary image
@@ -215,7 +241,8 @@ void getContourLine(Mat& img, vector<Mat>& objList, int thresh, int k){
  */
 void plotFD(Mat& fd, string win, double dur) {
 
-	// TODO
+  //todo
+
 }
 
 /**
@@ -226,16 +253,16 @@ void plotFD(Mat& fd, string win, double dur) {
  */
 void showImage(Mat& img, string win, double dur) {
 
-	// use copy for normalization
-	Mat tempDisplay = img.clone();
-	if (img.channels() == 1) {
-		normalize(img, tempDisplay, 0, 255, CV_MINMAX);
-	}
-	// create window and display omage
-	namedWindow(win.c_str(), CV_WINDOW_AUTOSIZE);
-	imshow(win.c_str(), tempDisplay);
-	// wait
-	if (dur >= 0) {
-		waitKey(dur);
-	}
+    // use copy for normalization
+    Mat tempDisplay = img.clone();
+    if (img.channels() == 1) {
+        normalize(img, tempDisplay, 0, 255, CV_MINMAX);
+    }
+    // create window and display omage
+    namedWindow(win.c_str(), CV_WINDOW_AUTOSIZE);
+    imshow(win.c_str(), tempDisplay);
+    // wait
+    if (dur >= 0) {
+        waitKey(dur);
+    }
 }
