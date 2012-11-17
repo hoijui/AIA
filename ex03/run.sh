c51c471c3feeaa7263ee4669fb66471b4c05ac6e
Robin Vobruba <robin.vobruba@gmail.com>
2012-11-17 09:50:35 +0100

    ex03: adjust the run script to use the given image file names

--- ex03/run.sh
+++ ex03/run.sh
@@ -11,8 +11,18 @@ if [ ! -f "${EXECUTABLE}" ]; then
 	./build.sh
 fi
 
+if [ ! -f "${RESOURCES_DIR}/img/poker.jpg" ]; then
+	if [ ! -f aia3-material.zip ]; then
+		echo "Please download the additional materials (aia3-material.zip) from the AIA course ISIS page"
+		exit 1
+	fi
+	mkdir -p "${RESOURCES_DIR}"
+	cd "${RESOURCES_DIR}"
+	unzip "../../../aia3-material.zip"
+fi
+
 cd "${TARGET_DIR}"
 "${EXECUTABLE}" \
-	"${RESOURCES_DIR}/img/template.jpg" \
-	"${RESOURCES_DIR}/img/test.jpg"
+	"${RESOURCES_DIR}/img/moneyTemplate100.jpg" \
+	"${RESOURCES_DIR}/img/poker.jpg"
 
