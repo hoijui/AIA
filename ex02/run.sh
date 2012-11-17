60df3b341aa288b0e92e541480c1c594c0b3a4e6
Robin Vobruba <robin.vobruba@gmail.com>
2012-11-17 09:00:23 +0100

    ex02: improve build & run scripts a bit

--- ex02/run.sh
+++ ex02/run.sh
@@ -3,13 +3,16 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 TARGET_DIR="${SCRIPT_DIR}/target"
 RESOURCES_DIR="${SCRIPT_DIR}/src/main/resources"
+EXECUTABLE="${TARGET_DIR}/aia2"
 
 cd "${SCRIPT_DIR}"
 
-./build.sh
+if [ ! -f "${EXECUTABLE}" ]; then
+	./build.sh
+fi
 
 cd "${TARGET_DIR}"
-./aia2 \
+"${EXECUTABLE}" \
 	"${RESOURCES_DIR}/img/orig.jpg" \
 	"${RESOURCES_DIR}/img/blatt_art1.jpg" \
 	"${RESOURCES_DIR}/img/blatt_art2.jpg"
